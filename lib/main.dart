841f42b0ad9c5b27df0893ddebcee349611b727f
Panda Chan <huytq1@gmail.com>
2018-12-26 21:22:09 +0700

    add lake image

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Food Paradise')
-            ],
-          ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {
+
+                },
+                child: Text('Add Product'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text('Food Paradise')
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
